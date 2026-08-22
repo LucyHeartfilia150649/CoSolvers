c2592f858c612cedc3006198f3586cb630902486
Nattanun <chs1659902340892@chs.ac.th>
2026-08-22 09:32:33 +0700

    Update apps.py

--- COSOLVERS_app/apps.py
+++ COSOLVERS_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ViableGraphAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "viable_graph_app"
+    name = "COSOLVERS_app"
