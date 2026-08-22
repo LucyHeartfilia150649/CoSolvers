5235fb0d52c0d969ae997c17d9296467976c3339
Nattanun <chs1659902340892@chs.ac.th>
2026-08-22 21:10:31 +0700

    Start.sh Updates

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /app/COSOLVERS_project
 export PYTHONPATH=/app/COSOLVERS_project
-python manage.py migrate --run-syncdb || true
-exec gunicorn viable_graph_project.wsgi --bind 0.0.0.0:${PORT:-8080}
+python /app/COSOLVERS_project/manage.py migrate --run-syncdb || true
+exec gunicorn --chdir /app/COSOLVERS_project viable_graph_project.wsgi --bind 0.0.0.0:${PORT:-8080}
