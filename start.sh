da1fb0578444c4a9bca1a0a7e22fffd3dcd29058
Nattanun <chs1659902340892@chs.ac.th>
2026-08-22 21:01:32 +0700

    fix: rewrite start.sh correctly

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
-﻿@'
-#!/bin/bash
+﻿#!/bin/bash
 set -e
 cd /app/COSOLVERS_project
 python manage.py migrate --run-syncdb || true
 exec gunicorn viable_graph_project.wsgi --bind 0.0.0.0:${PORT:-8080}
-'@ | Set-Content -Encoding utf8 start.sh
